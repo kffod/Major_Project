5d023e4e5770d4bcfb2a9fcab7a94f7bef882f11
Sachin Jadhav <98410437+kffod@users.noreply.github.com>
2025-03-28 01:48:48 +0530

    Delete Reddit Frontend and backend directory

--- Reddit Frontend and backend/backend/config.py
+++
@@ -1,4 +0,0 @@
-# Reddit API Credentials
-REDDIT_CLIENT_ID = "i9bY5so8H71m5yGQcBjHiw"
-REDDIT_CLIENT_SECRET = "aVIBtnEqrNub8jiUWkfuEVEB1AhGvg"
-REDDIT_USER_AGENT = "bot-detection-script"
--- Reddit Frontend and backend/backend/utils/preprocessing.py
+++
@@ -1,6 +0,0 @@
-def preprocess_data(input_data):
-    return [
-        input_data.get("post_karma", 0),
-        input_data.get("comment_karma", 0),
-        input_data.get("listed_count", 0)
-    ]
